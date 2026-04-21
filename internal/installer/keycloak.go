599683fab06bb3f9b5f23bce572f5fa1bce249c1
Gilmar Araujo <gilmararaujo@Gilmars-MacBook-Pro.local>
2026-04-21 12:31:26 -0300

    Fix KC_HOSTNAME_ADMIN to use full URL https://keycloak.local

--- internal/installer/keycloak.go
+++ internal/installer/keycloak.go
@@ -316,7 +316,7 @@ spec:
         - name: KC_HOSTNAME
           value: keycloak.local
         - name: KC_HOSTNAME_ADMIN
-          value: keycloak.local
+          value: https://keycloak.local
         - name: KC_HOSTNAME_STRICT
           value: "false"
         - name: KC_HTTP_PORT
