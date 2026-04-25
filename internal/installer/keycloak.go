b39a978c469fe17126bf7104f4d8658f20096f3e
Gilmar Araujo <gilmararaujo@Gilmars-MacBook-Pro.local>
2026-04-25 15:56:40 -0300

    Fix gosec lint: exclude G107 and G101 globally in golangci config

--- internal/installer/keycloak.go
+++ internal/installer/keycloak.go
@@ -212,7 +212,7 @@ func (k *Keycloak) deployKeycloak(creds keycloakCreds) error {
 	secrets := `apiVersion: v1
 kind: Namespace
 metadata:
-  name: keycloak` //nolint:gosec
+  name: keycloak`
 
 	rest := `
 ---
